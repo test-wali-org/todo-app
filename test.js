1cbb4678fb9befcf8f09ec22285fd7f54b190f1e
test-user <test@gmail.com>
2026-03-31 16:14:56 +0530

    feat: test pr 2

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 const DATABASE_URL = "postgres://postgres:postgres@18.18.81.10:5432/postgres";
 const ANOTHER_SECRET = "postgres://postgres:postgres@18.18.81.10:5432/postgres-db";
+const ANOTHER_SECRET2 = "postgres://postgres:postgres@18.18.81.10:5432/postgres-db";
